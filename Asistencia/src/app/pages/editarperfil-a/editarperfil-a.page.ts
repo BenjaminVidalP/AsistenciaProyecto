da1a26ae711f03d85e446a5d32340dba3abe3178
BenjaminVidalP <be.vidalp@duocuc.cl>
2022-10-15 14:34:21 -0300

    camara_editarperfil2.4

--- Asistencia/src/app/pages/editarperfil-a/editarperfil-a.page.ts
+++ Asistencia/src/app/pages/editarperfil-a/editarperfil-a.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TomarFotoService } from 'src/app/services/tomar-foto.service';
 
 @Component({
   selector: 'app-editarperfil-a',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editarperfil-a.page.scss'],
 })
 export class EditarperfilAPage implements OnInit {
-
-  constructor() { }
+  imageData: any;
+  constructor(private c: TomarFotoService) { }
+  tomarF(){
+    this.c.takePicture();
+  }
 
   ngOnInit() {
+    this.c.regresarfoto().subscribe((res) => {
+      this.imageData = res;
+    })
   }
 
 }
