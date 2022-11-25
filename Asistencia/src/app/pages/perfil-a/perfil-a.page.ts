9d56b60ae51ffbcfa7cde5b523dbfe8c4c2f62fc
YzanBaezz <101334205+YzanBaezz@users.noreply.github.com>
2022-11-25 10:46:31 -0300

    cambios-perfil-a
    intento prueba perfil-a

--- Asistencia/src/app/pages/perfil-a/perfil-a.page.ts
+++ Asistencia/src/app/pages/perfil-a/perfil-a.page.ts
@@ -61,6 +61,7 @@ export class PerfilAPage implements OnInit {
       if (res) {
         this.imageData = res;
         this.servicio.actualizarFoto(this.imageData, this.id_pe);
+        this.servicio.TraerPerfiles();
       }
     })
     this.servicio.dbState().subscribe((res) =>{
@@ -71,8 +72,8 @@ export class PerfilAPage implements OnInit {
       }
       this.token=localStorage.getItem('perfiles')
       console.log("Hola " + this.token)
-      for (let i = 0; i < this.usuario.length; i++) {
-      if(this.usuario[i].id_perfil_usuario == this.token ){
+      for (let i = 0; i < this.perfil.length; i++) {
+      if(this.perfil[i].id_perfil_usuario == this.token ){
         this.id_usuario = this.perfil[i].id_usuario
         this.nombre = this.perfil[i].nombre
         this.apellido = this.perfil[i].apellido
